620e4e95e347df76f7516e285c602f0615d4f499
Moshe Zadka <zadka.moshe@gmail.com>
2020-11-01 16:20:52 -0800

    read

---
+++ src/starlog/read.py
@@ -0,0 +1,20 @@
+import datetime
+import pathlib
+import textwrap
+import sys
+
+def ctime(log):
+    return datetime.datetime.fromtimestamp(log.stat().st_ctime)
+
+def print_logs(path, current, outfile):
+    cutoff =  current - datetime.timedelta(days=7)
+    recent_files = [
+        log
+        for log in path.glob("*.txt")
+        if ctime(log) > cutoff
+    ]
+    recent_files.sort()
+    for log in recent_files:
+        text = log.read_text()
+        text = textwrap.fill(text)
+        print(log.name, "\n\n", text, "\n", file=outfile)
